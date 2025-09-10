15c3f20c0b0e0f31d131e06152cd35c83a2e1c6d
JuliaSelua <104761063+JuliaSelua@users.noreply.github.com>
2025-09-10 02:12:27 +0200

    Update networks_stylegan2.py

--- training/networks_stylegan2.py
+++ training/networks_stylegan2.py
@@ -616,6 +616,11 @@ class Generator(torch.nn.Module):
         img = self.synthesis(ws_combined, update_emas=update_emas, **synthesis_kwargs)
         return img
 
+        def forward_for_fid(self, z, c=None, truncation_psi=1, **kwargs):
+            z_id = z[:, :self.z_dim_id]
+            z_style = z[:, self.z_dim_id:]
+            return self.forward(z_id, z_style, c, truncation_psi=truncation_psi, **kwargs)
+
 #----------------------------------------------------------------------------
 '''
 # Baseline Generator
