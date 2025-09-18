3de23e5a23c4906496eb323ffbed17cbe2674935
JuliaSelua <104761063+JuliaSelua@users.noreply.github.com>
2025-09-18 22:19:04 +0200

    Update loss.py

--- training/loss.py
+++ training/loss.py
@@ -149,7 +149,6 @@ class StyleGAN2Loss(Loss):
         if phase in ['Gmain', 'Gboth']:
             with torch.autograd.profiler.record_function('Gmain_forward'):
                 gen_img, _gen_ws = self.run_G(gen_z, gen_z2, gen_c)             #Generator Forward for whole batch
-                print("gen_img.shape:", gen_img.shape)
                 gen_logits = self.run_D(gen_img, gen_c, blur_sigma=blur_sigma)
                 training_stats.report('Loss/scores/fake', gen_logits)           #GAN loss 
                 training_stats.report('Loss/signs/fake', gen_logits.sign())
