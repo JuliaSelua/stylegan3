13bb0ef1f7a76c03a722f9936064866035319c7c
JuliaSelua <104761063+JuliaSelua@users.noreply.github.com>
2025-09-18 22:04:52 +0200

    Update loss.py

--- training/loss.py
+++ training/loss.py
@@ -13,6 +13,7 @@ import torch
 from torch_utils import training_stats
 from torch_utils.ops import conv2d_gradfix
 from torch_utils.ops import upfirdn2d
+import torch.nn.functional as F
 
 import cv2
 import torch
