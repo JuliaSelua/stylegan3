4e17e48d46a97ed763da32ba71effc68df149790
JuliaSelua <104761063+JuliaSelua@users.noreply.github.com>
2025-09-27 00:32:58 +0200

    Update networks_stylegan2.py

--- training/networks_stylegan2.py
+++ training/networks_stylegan2.py
@@ -542,8 +542,9 @@ class Generator(torch.nn.Module):
         self.img_channels = img_channels
         self.synthesis = SynthesisNetwork(w_dim=2*w_dim, img_resolution=img_resolution, img_channels=img_channels, **synthesis_kwargs)
         self.num_ws = self.synthesis.num_ws
-        self.mapping = MappingNetwork(z_dim=z_dim, c_dim=c_dim, w_dim=w_dim, num_ws=self.num_ws, **mapping_kwargs)
-        self.mapping2 = MappingNetwork(z_dim=z_dim, c_dim=c_dim, w_dim=w_dim, num_ws=self.num_ws, **mapping_kwargs)
+        num_ws_per_mapping = self.num_ws // 2
+        self.mapping = MappingNetwork(z_dim=z_dim, c_dim=c_dim, w_dim=w_dim, num_ws=num_ws_per_mapping, **mapping_kwargs)
+        self.mapping2 = MappingNetwork(z_dim=z_dim, c_dim=c_dim, w_dim=w_dim, num_ws=num_ws_per_mapping, **mapping_kwargs)
 
 
     def forward(self, z, c, z2=None, truncation_psi=1, truncation_cutoff=None, update_emas=False, **synthesis_kwargs):
