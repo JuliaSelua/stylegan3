73eaa689466a5cd56f987af17df7fafbe03356bb
JuliaSelua <104761063+JuliaSelua@users.noreply.github.com>
2025-10-06 22:54:44 +0200

    Update loss.py

--- training/loss.py
+++ training/loss.py
@@ -156,7 +156,7 @@ class StyleGAN2Loss(Loss):
 
     def accumulate_gradients(self, phase, real_img, real_c, gen_z, gen_z2, gen_c, gain, cur_nimg):
         lambda_id = 1.0
-        lambda_style = 0.1
+        lambda_style = 1.0
         assert phase in ['Gmain', 'Greg', 'Gboth', 'Dmain', 'Dreg', 'Dboth']
         if self.pl_weight == 0:
             phase = {'Greg': 'none', 'Gboth': 'Gmain'}.get(phase, phase)
