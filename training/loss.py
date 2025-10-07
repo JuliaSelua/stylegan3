d0aa8b3f8bef237a50ea3f3a19d2d77af4ad5371
JuliaSelua <104761063+JuliaSelua@users.noreply.github.com>
2025-10-08 00:20:22 +0200

    Add debug

--- training/loss.py
+++ training/loss.py
@@ -180,6 +180,7 @@ class StyleGAN2Loss(Loss):
                 if self.use_id_loss:
                     emb_a, emb_b = emb[0::2], emb[1::2]
                     id_loss = (1 - (emb_a * emb_b).sum(dim=1)).mean()
+                    print("DEBUG: Erste 4 ID-Paare Cosine-Similarity:", ((emb_a * emb_b).sum(dim=1)[:4]).cpu().numpy())
                     loss_Gmain = loss_Gmain + lambda_id * id_loss
                     training_stats.report('Loss/G/id_loss', id_loss)
     
@@ -193,6 +194,7 @@ class StyleGAN2Loss(Loss):
                 if self.use_style_loss:
                     img_a, img_b = gen_img[0::2], gen_img[1::2]
                     style_loss = self.style_loss_fn(img_a, img_b)
+                    print("DEBUG: Erste 4 Style-Loss Werte (LPIPS):", style_loss[:4].cpu().numpy())
                     loss_Gmain = loss_Gmain + lambda_style * style_loss
                     training_stats.report('Loss/G/style_loss', style_loss)
 
