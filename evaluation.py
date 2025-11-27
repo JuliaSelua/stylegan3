7a0b57e35efee98584c28c7ac205b274f230149f
JuliaSelua <104761063+JuliaSelua@users.noreply.github.com>
2025-11-27 12:09:32 +0100

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -208,6 +208,11 @@ legend = ax.get_legend()
 if legend is not None:
     legend.set_title(None)
 
+
+# Achsenlimits setzen
+ax.set_xlim(-1.1, 1.1)   # x-Achse von -1 bis 1
+ax.set_ylim(0, 0.8)  # y-Achse von 0 bis 0.8
+
 # Legende nur für genuine / imposter
 #plt.legend(title=None)
 
